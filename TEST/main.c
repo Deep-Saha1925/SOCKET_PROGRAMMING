ce555597135ba7b3ef980ffbf646dde037b86f37
Deep-Saha1925 <dip23447@gmail.com>
2026-06-06 22:18:28 +0530

    Establishing connection to google

--- TEST/main.c
+++ TEST/main.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<arpa/inet.h>
 
 int main(){
 
     int socketFD = socket(AP_INET, SOCK_STREAM, 0);
     
+    char* ip = "8.8.8.8";
     struct sockaddr_in address;
-    
+    address.sin_family = AF_INET;
+    address.sin_port = htons(80);
+    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
 
-    connect(socketFD, &address, sizeof address);
+    int result = connect(socketFD, &address, sizeof address);
+
+    if(result == 0){
+        printf("CONNECTION SUCCESSFUL\n");
+    }
 
     return 0;
 }
