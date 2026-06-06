f874bae412b7b5e3114f0d8e8b40edc648f7a43e
Deep-Saha1925 <dip23447@gmail.com>
2026-06-06 23:04:52 +0530

    Sending request to google

--- TEST/main.c
+++ TEST/main.c
@@ -2,12 +2,13 @@
 #include<sys/socket.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<string.h>
 
 int main(){
 
     int socketFD = socket(AP_INET, SOCK_STREAM, 0);
     
-    char* ip = "8.8.8.8";
+    char* ip = "172.217.24.14";
     struct sockaddr_in address;
     address.sin_family = AF_INET;
     address.sin_port = htons(80);
@@ -18,6 +19,16 @@ int main(){
     if(result == 0){
         printf("CONNECTION SUCCESSFUL\n");
     }
+
+    char* buffer;
+    buffer = "GET \\ HTTP /1.1\r\nHost:google.com\r\n\r\n";
+    send(socketFD, buffer, strlen(buffer), 0);
+
+    char buffer[1024];
+    recv(socketFD, buffer, 1024, 0);
+
+    printf("Response was %s\n", buffer);
+
 
     return 0;
 }
