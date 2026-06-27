6c031edceabcbb0dd84f33b6d8dda32476d1e579
Deep-Saha1925 <dip23447@gmail.com>
2026-06-27 22:12:53 +0530

    Updated main file with connection format

--- TEST/main.c
+++ TEST/main.c
@@ -3,18 +3,27 @@
 #include<netinet/in.h>
 #include<arpa/inet.h>
 #include<string.h>
+#include<malloc.h>
+
+int createTCPIpv4Socket(){
+    return socket(AP_INET, SOCK_STREAM, 0);
+}
+
+struct sockaddr_in* createTCPIpv4Address(char* ip, int port){
+    struct sockaddr_in *address = malloc(sizeof(struct sockaddr_in));
+    address->sin_family = AF_INET;
+    address->sin_port = htons(port);
+    inet_pton(AF_INET, ip, &address->sin_addr.s_addr);
+    return address;
+}
 
 int main(){
 
-    int socketFD = socket(AP_INET, SOCK_STREAM, 0);
+    int socketFD = createTCPIpv4Socket();
     
-    char* ip = "172.217.24.14";
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_port = htons(80);
-    inet_pton(AF_INET, ip, &address.sin_addr.s_addr);
+    struct sockaddr_in *address = createTCPIpv4Address("172.217.24.14", 80);
 
-    int result = connect(socketFD, &address, sizeof address);
+    int result = connect(socketFD, address, sizeof(*address));
 
     if(result == 0){
         printf("CONNECTION SUCCESSFUL\n");
